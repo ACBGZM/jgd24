2aac0bd2f977d8ec7fdd3c1e53c2a28e3bdd3f53
BianbBb <34503772+BianbBb@users.noreply.github.com>
2024-10-27 02:56:21 +0800

    GlobalLight (#16)
    Co-authored-by: BBBB <>

---
+++ ProjectLight/Assets/Scripts/Damage/HealthManager.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.UI;
+
+public class HealthManager : MonoBehaviour
+{   
+    //
+    public GameObject player;
+    public GameObject boss;
+
+    // 血条
+    [Header("血条")]
+    public Slider playerHealthSlider;
+    public Slider bossHealthSlider;
+
+    //
+    [Header("全局光照")]
+    public Light2D globalLight;
+    public float lightIntensity=0.02f;
+    public float stageTwoHealthThreshold = 0.4f;
+   
+    private void OnEnable()
+    {
+        HealthChangeEvent.OnHealthChanged += ResponseHealthChanged;
+    }
+
+    private void OnDisable()
+    {
+        HealthChangeEvent.OnHealthChanged -= ResponseHealthChanged;
+    }
+
+    public void ResponseHealthChanged(int newHealth, GameObject sourceObject)
+    {
+        if (sourceObject == player)
+        {
+            // playerHealthSlider.maxValue = player.GetComponent<PlayerOnHit>().maxHealth;
+            // playerHealthSlider.value = player.GetComponent<PlayerOnHit>().Health;
+            playerHealthSlider.value = newHealth;
+
+        }
+        else if (sourceObject == boss)
+        {
+            // bossHealthSlider.maxValue = boss.GetComponent<BossOnHit>().maxHealth;
+            // bossHealthSlider.value = boss.GetComponent<BossOnHit>().Health;
+            bossHealthSlider.value = newHealth; //UpdateHealthBar
+
+            float curHealthRate = newHealth/boss.GetComponent<BossOnHit>().Health;
+            if(curHealthRate <= stageTwoHealthThreshold)
+            {
+                globalLight.intensity = lightIntensity;
+                // 其他灯光设置
+            } 
+
+
+        }
+    }
+
+    // public void UpdateHealthBar()
+    // {
+         // 
+    // boss.GetComponent<BossOnHit>().Health;
+    // }
+
+    // public void SwitchGlobalLight()
+    // {
+
+    // }
+
+
+}
