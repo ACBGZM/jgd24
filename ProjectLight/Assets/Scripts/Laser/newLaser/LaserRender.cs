1848a8fcfe6ec303454864f66379d43c93eca499
DESKTOP-CCL570M\-BBBBB- <m18342211892@163.com>
2024-10-28 14:37:00 +0800

    vfx

--- ProjectLight/Assets/Scripts/Laser/newLaser/LaserRender.cs
+++ ProjectLight/Assets/Scripts/Laser/newLaser/LaserRender.cs
@@ -5,16 +5,25 @@ using UnityEngine;
 public class LaserRender : MonoBehaviour
 {
     public GameObject lineSegmentPrefab;
-    public GameObject startVFX;
-    public GameObject midVFX;
-    public GameObject endVFX;
-    public GameObject hitVFX;
+    public GameObject beamVFXPrefab;
+    // public GameObject startVFXPrefab;
+    // public GameObject midVFXPrefab;
+    // public GameObject endVFXPrefab;
+    // public GameObject hitVFXPrefab;
 
     // lines
     private GameObject line_1;
     private GameObject line_2;
+    private GameObject startVFX;
+    private GameObject midVFX;
+    private GameObject endVFX;
 
     public float lineWidth = 1.0f;
+    public Vector3 startVFXScale;
+    public Vector3 midVFXScale ;
+    public Vector3 endVFXScale ;
+
+    private Color midVFXColor;
 
     // public Material lineMaterial;
     public List<Vector2> lines;
@@ -26,34 +35,52 @@ public class LaserRender : MonoBehaviour
         //   = 
         line_1 = Instantiate(lineSegmentPrefab,Vector3.zero,Quaternion.identity,this.transform); 
         line_2 = Instantiate(lineSegmentPrefab,Vector3.zero,Quaternion.identity,this.transform); 
+        startVFX = Instantiate(beamVFXPrefab,Vector3.zero,Quaternion.identity,this.transform); 
+        midVFX = Instantiate(beamVFXPrefab,Vector3.zero,Quaternion.identity,this.transform); 
+        endVFX = Instantiate(beamVFXPrefab,Vector3.zero,Quaternion.identity,this.transform); 
+        startVFX.transform.localScale = startVFXScale;
+        midVFX.transform.localScale = midVFXScale;
+        endVFX.transform.localScale = endVFXScale;
+
+        midVFXColor = midVFX.GetComponent<ParticleSystemRenderer>().material.color;
+
 
     }
     // public void DrawLines(List<Vector2> keyPoints,Material material,bool laserStatus,float lineWidth)
     public void DrawLines(List<Vector2> keyPoints,bool laserStatus,float lineWidth)
     {      
-        // // Draw Segment
+       
 
         if(keyPoints.Count == 2)
         {   
+
+            // Draw Segment
             line_1.GetComponent<LaserLineSegment>().DrawLineSegment(keyPoints[0],keyPoints[1]);
             line_2.GetComponent<LineRenderer>().enabled = false;
+            // Draw VFX
+            startVFX.transform.position = keyPoints[0];
+            endVFX.transform.position = keyPoints[1];
+            midVFXColor.a = 0f;
+          
+
+
+
         }
         else if(keyPoints.Count == 3)
         {   
+             // Draw Segment
             line_1.GetComponent<LaserLineSegment>().DrawLineSegment(keyPoints[0],keyPoints[1]);
             line_2.GetComponent<LineRenderer>().enabled = true;
             line_2.GetComponent<LaserLineSegment>().DrawLineSegment(keyPoints[1],keyPoints[2]);
+
+            // Draw VFX 
+            startVFX.transform.position = keyPoints[0];
+            endVFX.transform.position = keyPoints[2];
+            midVFX.transform.position = keyPoints[1];
+            midVFXColor.a = 1f;
+
+
         }
-
-
-        // // Draw VFX
-
-        // if(keyPoints.Count == 3)
-        // {
-        //     // 画中点
-        // }
-
-
 
 
         // Debug.Log("激光关键点");
